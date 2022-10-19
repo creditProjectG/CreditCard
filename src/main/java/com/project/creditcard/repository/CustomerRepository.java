cae0aa28493cfc7630022968c833689dc830bf7d
creditProjectG <116124418+creditProjectG@users.noreply.github.com>
2022-10-19 10:42:12 -0400

    Delete src directory

--- src/main/java/com/project/creditcard/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.creditcard.repository;
-
-import com.project.creditcard.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
